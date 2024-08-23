69bedf3f23155d68069f83f77966335741a5472c
pro-use <robertprouse@gmail.com>
2024-08-23 14:01:37 +0100

    first commit

---
+++ site/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+        'kql' => [
+            'auth' => 'bearer'
+        ],
+        'headless' => [
+            'token' => 'Jg208;rir!-c',
+            'cors' => [
+                'allowOrigin' => '*',
+                'allowMethods' => 'GET, POST, OPTIONS',
+                'allowHeaders' => 'Accept, Content-Type, Authorization, X-Language',
+                'maxAge' => '86400',
+            ]
+        ]
+    ];
